69aa2823f00480a8c198560bbd3677ea19703b49
Mayank2335 <SHARMAYANK2056@GMAIL.COM>
2025-03-17 14:25:00 +0530

    designing_a_component_for_your_entity_assignment_2

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,20 @@
 // write product card here
+import Button from "./button";
+
+const Productcard = () => { 
+    const productName = 'Samsung S25 Ultra';
+    const productImage = 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRp4sOLNPxp1UBdM5TEJD-Qg_8-e0BdmfiMKWWItJ3dTCBjG7Wx3LSNMdIm04hysu5NybpClvH1Q3KnMJS-pnMfQkr_kaoAy7V5IZDJOvr79CMnXZ4nbBGb';
+    const price = 'Rs. 1,25,000';
+    
+    return(
+        <div>
+            <h3>Name of Product: {productName} </h3>
+            <img style={{width: "200px"}} src={productImage} alt="product image"/>
+            <p><strong>Price: {price} </strong></p>
+            <Button/>
+        </div>
+    )
+}
+
+
+export default Productcard;
